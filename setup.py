8c54b6272efa4c19fb800c3879ddd6ad70bef371
Chris Wilkins <cbw182@gmail.com>
2020-02-29 14:51:44 -0500

    Added initial setup.py and folder structure for library.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+script_version = 'pre-alpa'
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='q3serverquery',
+    version=script_version,
+    author='Chris Wilkins',
+    author_email='cbw182@gmail.com',
+    description='A library to query Quake 3 master servers and game servers.',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
